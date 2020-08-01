7e512da34adf34b56eeff4bb7c14b54b6ae71d56
Calla Lu <46143943+clu2001@users.noreply.github.com>
2020-08-01 19:39:47 -0400

    html coding

--- client/src/app/contacts/contacts.component.ts
+++ client/src/app/contacts/contacts.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../contact.service'; 
+import { Contact } from '../contact'; 
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.css'],
+  providers: [ContactService]
 })
 export class ContactsComponent implements OnInit {
+  contacts: Contact[]; 
+  contact: Contact; 
+  first_name: string; 
+  last_name: string; 
+  contact_number: string; 
 
-  constructor() { }
+  constructor(private contactService: ContactService) { }
 
-  ngOnInit(): void {
+
+  addContact() {
+    const newContact ={
+      first_name: this.first_name, 
+      last_name: this.last_name,
+      contact_number: this.contact_number
+    }
+    this.contactService.addContact(newContact)
+      .subscribe(contact => {
+        this.contacts.push(contact); 
+      }); 
+
+  }
+
+  deleteContact(id:any) {
+    var contacts = this.contacts; 
+    this.contactService.deleteContact(id)
+      .subscribe(data => {
+        if(data.n==1) {
+          for (var i = 0; i < this.contacts.length; i++) {
+            if (contacts[i]._id == id) {
+              contacts.splice(i, 1); 
+            }
+          }
+        }
+      })
+  }
+
+  ngOnInit() {
+    this.contactService.getContacts() 
+      .subscribe( contacts => this.contacts = contacts); 
   }
 
 }
